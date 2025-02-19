fd97b06576b6c424fe3968625dc8e1cb1717d3f3
vishal kumar <vishal.rathor1991@gmail.com>
2025-02-19 13:15:52 +0530

    ok

---
+++ frontend/src/components/LatestCollection.jsx
@@ -0,0 +1,28 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from './Title'
+
+const LatestCollection = () => {
+
+    const { products } = useContext(ShopContext);
+    const [latestProducts,setLatestProducts] = useState([]);
+
+    useEffect(()=>{
+        setLatestProducts(products.slice(0,10));
+    },[])
+
+    
+  return (
+    <div className='my-10'>
+      <div className='text-center py-8 text-3xl'>
+        <Title text1={'LATEST'} text2={'COLLECTIONS'} />
+        <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the.
+        </p>
+
+      </div>
+      
+    </div>
+  )
+}
+
+export default LatestCollection
